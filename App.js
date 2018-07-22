56c3949607017fd3d91aebd92a5c89fb79cbfc42
Jakhongir Khusanov <worldj95@gmail.com>
2018-07-21 21:40:03 -0700

    Redux initial library added along with Firebase

--- App.js
+++ App.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './app/reducers';
+import API_KEYS from './app/utils/config_keys';
 
 export default class App extends React.Component {
+  componentDidUpdate(){
+    firebase.initializeApp({
+      apiKey: API_KEYS[0].apiKey,
+      authDomain: API_KEYS[0].authDomain,
+      databaseURL: API_KEYS[0].databaseURL,
+      projectId: API_KEYS[0].projectId,
+      storageBucket: API_KEYS[0].storageBucket,
+      messagingSenderId: API_KEYS[0].messagingSenderId
+    });
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={createStore(reducers)}>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+        </View>
+      </Provider>
     );
   }
 }
